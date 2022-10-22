cb63bcf0b5f23f9377253a698f7e29172a1788db
ghiath2 <ghiathht3@gmail.com>
2022-10-22 22:59:21 +0000

    Add code in views to return spesific information

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from .views import TaskList, TaskDetail
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
--- base/views.py
+++ base/views.py
@@ -7,3 +7,9 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
