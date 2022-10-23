630ed9d6c1f9f7b8cde04a7e57d3cc3daa31a79d
ghiath2 <ghiathht3@gmail.com>
2022-10-23 01:53:14 +0000

    Clean and organise code

--- base/views.py
+++ base/views.py
@@ -3,17 +3,24 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
-from .models import Task
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
+# Imports for Reordering Feature
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
+
+from .models import Task
+from .forms import PositionForm
+
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
-    fields = 'title', 'description', 'complete'
+    fields = '__all__'
     redirect_authenticated_user = True
 
     def get_success_url(self):
@@ -32,7 +39,7 @@ class RegisterPage(FormView):
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
 
-        def get(self, *args, **kwargs):
+    def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect('tasks')
         return super(RegisterPage, self).get(*args, **kwargs)
@@ -53,10 +60,11 @@ class TaskList(LoginRequiredMixin, ListView):
                 title__contains=search_input)
 
         context['search_input'] = search_input
+
         return context
 
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
@@ -64,7 +72,7 @@ class TaskDetail(DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    field = 'title', 'description', 'complete'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
@@ -82,3 +90,20 @@ class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(user=owner)
+
+
+class TaskReorder(View):
+    def post(self, request):
+        form = PositionForm(request.POST)
+
+        if form.is_valid():
+            positionList = form.cleaned_data["position"].split(',')
+
+            with transaction.atomic():
+                self.request.user.set_task_order(positionList)
+
+        return redirect(reverse_lazy('tasks'))
