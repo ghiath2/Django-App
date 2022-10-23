fa7468c2d33f353d02811c58a209e23749f02240
ghiath2 <ghiathht3@gmail.com>
2022-10-23 06:25:19 +0000

    test

--- base/tests.py
+++ base/tests.py
@@ -1,18 +1,3 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
+from django.test import TestCase
 
-
-class Task(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        order_with_respect_to = 'user'
+# Create your tests here.
