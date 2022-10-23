39873bd235d13de896d4096f79cd06d1f84dcb57
ghiath2 <ghiathht3@gmail.com>
2022-10-23 00:29:36 +0000

    Add code to only allow users to see their data

--- base/views.py
+++ base/views.py
@@ -22,6 +22,12 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+
 
 class TaskDetail(DetailView):
     model = Task
@@ -29,19 +35,19 @@ class TaskDetail(DetailView):
     template_name = 'base/task.html'
 
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     field = '__all__'
     success_url = reverse_lazy('tasks')
 
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
