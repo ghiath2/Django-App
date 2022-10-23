439193251fd5f8c1eb5fb567d08fcf99ed022c27
ghiath2 <ghiathht3@gmail.com>
2022-10-23 00:07:37 +0000

    Create login html,import LoginView and add class CustomLoginView

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create', TaskCreate().as_view(), name='task-create'),
