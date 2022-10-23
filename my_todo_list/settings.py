c8e4e38f50bed818843659f08348303d35f65848
ghiath2 <ghiathht3@gmail.com>
2022-10-23 05:58:08 +0000

    Test

--- my_todo_list/settings.py
+++ my_todo_list/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p_91)@mm@mk#t!z2*(cc1)m=12$#_&so2vnrf9w3=$y-c^xa(k'
+SECRET_KEY = 'django-insecure-p_91)@mm@mk#t!z2* \
+              (cc1)m=12$#_&so2vnrf9w3=$y-c^xa(k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ckz8780-django-my-todo.herokuapp.com']
 
@@ -87,16 +88,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation \
+                .UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth \
+            .password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth \
+            .password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth \
+        .password_validation.NumericPasswordValidator',
     },
 ]
 
